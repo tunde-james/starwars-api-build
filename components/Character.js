e2ccaf201080989ce229e3e69f3578362779ae2f
Tunde James <tundejames04@gmail.com>
2022-01-13 15:27:39 +0100

    Created project structure and homepage start up code

---
+++ components/Character.js
@@ -0,0 +1,7 @@
+export default function Character() {
+  return (
+    <div>
+      <h1>CHARACTER</h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,54 @@
 import styled from 'styled-components'
+import Head from 'next/head'
+import { useState } from 'react'
+import Character from '../components/Character'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export default function Characters({ characters }) {
+  const [value, setValue] = useState(12)
 
-export default function Home() {
-  return <Title>My page</Title>
+  const handleShowMore = () => {
+    setValue(value + 12)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Star Wars Character | StarWars.com</title>
+      </Head>
+      <HomeScreenContainer>
+        <StarfieldLeft />
+        <CharacterContainer>
+          {characters
+            .filter((character) => character.id !== 28 && character.id !== 77)
+            .map(
+              (character, index) =>
+                index < value && (
+                  <Character character={character} key={character.id} />
+                )
+            )}
+        </CharacterContainer>
+        <StarfieldRight />
+      </HomeScreenContainer>
+    </>
+  )
 }
+
+export async function getStaticProps(context) {
+  const characters = await fetch(
+    `https://akabab.github.io/starwars-api/api/all.json`
+  ).then((res) => res.json())
+
+  return {
+    props: {
+      characters,
+    },
+  }
+}
+
+const HomeScreenContainer = styled.div``
+
+const StarfieldLeft = styled.div``
+
+const StarfieldRight = styled.div``
+
+const CharacterContainer = styled.div``
